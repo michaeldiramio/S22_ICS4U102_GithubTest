48ebae371f5107eebdad9dc99a9a970a9bf1c47a
hadamekl <106603760+hadamekl@users.noreply.github.com>
2022-05-31 14:32:49 +0000

    I added a shark

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
 		animals.add(new Armadilo());
     animals.add(new Giraffe());
     animals.add(new SeaOtter());
-
+    animals.add(new Shark());
 
 
 		for(int i = 0; i < animals.size(); i++) {
